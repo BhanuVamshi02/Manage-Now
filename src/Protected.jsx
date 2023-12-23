1231ae4bff60e4bdd35609def1de4a863cba1099
Bhanu Vamshi Anumula <bhanuvamshi0211@gmail.com>
2023-12-23 15:29:46 +0530

    setting Protected Routes

--- src/Protected.jsx
+++ src/Protected.jsx
@@ -4,9 +4,12 @@ import { Context } from "./Context/AuthContext";
 
 export const Protected = ({ children }) => {
   const { user } = useContext(Context);
-  if (!user) {
-    return <Navigate to="/" />;
-  } else {
+
+  if (user) {
+    // Case 1: If user is present, allow access to children (DashboardPage)
     return children;
+  } else {
+    // Case 2: If user is not present, navigate to the specified route (default is "/")
+    return <Navigate to="/" />;
   }
 };
